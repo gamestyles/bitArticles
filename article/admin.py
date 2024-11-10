b0d832746d065a8f29c262a95e8a90d45f67d8e3
Ali Es <alireza.esm98@gmail.com>
2024-11-10 12:17:11 +0330

    feat: add article, article score models with admin

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, ArticleScore
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at")
+    readonly_fields = ("created_at", )
+
+
+class ArticleScoreAdmin(admin.ModelAdmin):
+    list_display = ("id", "article", "user_id", "score", "created_at", "is_suspicious")
+    list_select_related = ("article", )
+    readonly_fields = ("created_at", )
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleScore, ArticleScoreAdmin)
--- article/models.py
+++ article/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.translation import gettext_lazy as _
 
 
 class Article(models.Model):
+    title = models.CharField(null=False, blank=False, max_length=255, help_text=_("the title of the article"))
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ArticleScore(models.Model):
     """
-    Articles!
+    User scores articles
     """
-    title = models.CharField(max_length=255)
+    article = models.ForeignKey(Article, on_delete=models.DO_NOTHING, help_text=_("score related to an article"))
+    user_id = models.UUIDField(null=False, blank=False, db_index=True, help_text=_("the user id who scored"))
+    score = models.PositiveSmallIntegerField(
+        null=False, blank=False, validators=[MaxValueValidator(5), MinValueValidator(1)],
+        help_text=_("the score that can be between 1 to 5")
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_suspicious = models.BooleanField(default=False, help_text=_("the score can be suspicious based on user activity"))
